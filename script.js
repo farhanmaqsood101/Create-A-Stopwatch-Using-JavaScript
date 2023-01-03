ecce727d18a75f160b4be9755aa686628bbcffbc
Farhan Maqsood <farhanmaqsood10@hotmail.com>
2023-01-03 23:41:52 +0500

    fourth commit

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function stopwatch(){
             hours++;
         }
     }
+
     let h = hours < 10 ? "0" + hours : hours;
     let m = minutes < 10 ? "0" + minutes : minutes;
     let s = seconds < 10 ? "0" + seconds : seconds;
-
     displayTime.innerHTML = h + ":" + m + ":" + s;
 }
 
@@ -30,6 +30,7 @@ function watchStart(){
 function watchStop(){
     clearInterval(timer);
 }
+
 function watchReset(){
     clearInterval(timer);
     [seconds , minutes , hours] = [0,0,0];
